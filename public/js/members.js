e75c758aebe9fe697944f1aa91243a928f3e0d09
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2019-06-21 10:10:51 -0500

    Starting to pour information in 'members' from 'members.js' file.

--- public/js/members.js
+++ public/js/members.js
@@ -13,6 +13,63 @@ $(document).ready(function () {
 
       console.log(data);
 
+      Object.keys(data).forEach(function (item) {
+        console.log(data[item].projects);
+
+        // ====================================== Elvan 
+
+        var $projectDiv = $("#projectDiv");
+        var $userNameBanner = $("#userNameBanner");
+
+        var user = data[0].user;
+        var projectCard;
+
+
+        // " +  + "
+
+        projectCard =
+          "<div class='card bg-dark text-white projectCard' " +
+          "style=' margin:5px; min-width: 120px' " +
+          "data-id='" + data[item].projects_id + "'>" +
+          "<div class='card-header'>Project #" + data[item].projects_id +
+          "</div> " +
+          "<div class='card-body'> " +
+          "<h6 class='card-title'> " +
+          data[item].projects +
+          "</h6> " +
+          "<p class='card-text'> " +
+          "<small class='text-muted'> " +
+          "Last updated 3 mins ago" +
+          "</small> " +
+          "</p> " +
+          "</div> " +
+          "</div>";
+
+        $userNameBanner.html(
+          "<b>Team Organizer™</b>" + 
+          "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + 
+          "Welcome: " + user);
+        $projectDiv.append(projectCard);
+
+      })
+
+      // ====================================== Elvan 
+
+      // ====================================== Manu 
+
+
+      // ====================================== Manu 
+
+
+
+      // ====================================== Nacho 
+
+
+      // ====================================== Nacho 
+
+
+
+
     });
 
 });
