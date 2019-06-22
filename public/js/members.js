68516d9776d14564eaacb2bc5094bf6f1b7dcd1b
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2019-06-22 01:45:05 -0500

    Get ready to pull.

--- public/js/members.js
+++ public/js/members.js
@@ -16,83 +16,79 @@ $(document).ready(function () {
 
       console.log(data);
 
+      var project = "first";
+
       Object.keys(data).forEach(function (item) {
         // console.log(data[item].projects);
 
-        // ====================================== Elvan 
+        if (project !== item.projects_id) {
+          console.log(item);
 
-        var $projectDiv = $("#projectDiv");
-        var $userNameBanner = $("#userNameBanner");
+          var $projectDiv = $("#projectDiv");
+          var $userNameBanner = $("#userNameBanner");
 
-        var user = data[0].user;
-        var projectCard;
+          var user = data[0].user;
+          var projectCard;
+
+          projectCard =
+            // Manuel CSS a las cards
+            "<div class='card  bg-secondary projectCard col-md-5 overflow-auto' " +
+            // "style=' margin:5px; min-width: 120px' " +
+            "data-id='" + data[item].projects_id + "'>" +
+            "<div class='card-header'>P id: " +
+            data[item].projects_id +
+            " - " + data[item].projects +
+            "</div> " +
+            "<div class='card-body'> " +
+            "<h6 class='card-title'> " +
+            data[item].project_description +
+            "</h6> " +
+            "<p class='card-text'> " +
+            "<small class='text-dark'> " +
+            "Last updated 3 mins ago" +
+            "</small> " +
+            "</p> " +
+            "</div> " +
+            "</div>";
+
+          $userNameBanner.html(
+            "<b>Team Organizer™</b>" +
+            "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" +
+            "Welcome: " + user);
+          $projectDiv.append(projectCard);
+
+        } else {
+
+          project = item.projects_id;
 
 
-        // " +  + "
+        }
 
-        projectCard =
-         // Manuel CSS a las cards
-        "<div class='card  bg-secondary projectCard col-md-5 overflow-auto' " +
-        // "style=' margin:5px; min-width: 120px' " +
-          "data-id='" + data[item].projects_id + "'>" +
-          "<div class='card-header'>P id: " 
-          + data[item].projects_id +
-          " - " + data[item].projects +
-          "</div> " +
-          "<div class='card-body'> " +
-          "<h6 class='card-title'> " +
-          data[item].project_description +
-          "</h6> " +
-          "<p class='card-text'> " +
-          "<small class='text-dark'> " +
-          "Last updated 3 mins ago" +
-          "</small> " +
-          "</p> " +
-          "</div> " +
-          "</div>";
+      });
 
-        $userNameBanner.html(
-          "<b>Team Organizer™</b>" + 
-          "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + 
-          "Welcome: " + user);
-        $projectDiv.append(projectCard);
+      $('.projectCard').on('click', function () {
+        $('.card').removeClass('border border-primary');
+        $(this).addClass('border border-primary');
 
-
-        $('.projectCard').on('click', function () {
-          $('.card').removeClass('border border-primary');
-          $(this).addClass('border border-primary');
-
-          var all = $('.border-primary').map(function () {
-            return this;
+        var all = $('.border-primary').map(function () {
+          return this;
         }).get();
-  
-        var id = $(all[0]).data('id');
 
-        $('#forProject').text(id);
-      
+        var categoryId = $(all[0]).data('id');
+
+        // $('#forProject').text(id);
+        $.ajax({
+            url: "/members/info/" + categoryId,
+            method: "GET"
+          })
+          .then(function (data) {
+
+            console.log(data);
+
+          });
+
       })
-
-      
-      })
-
-      // ====================================== Elvan 
-
-      // ====================================== Manu 
-
-
-      // ====================================== Manu 
-
-
-
-      // ====================================== Nacho 
-
-
-      // ====================================== Nacho 
-
-
-
 
     });
 
 });
-
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -101,4 +101,64 @@ module.exports = function (app) {
 
   });
 
+  app.get("/members/info/:categoryId", isAuthenticated, function (req, res) {
+    // function test(cb) {
+
+    var userId = req.user.id;
+    var categoryId = req.params.categoryId;
+
+    console.log(userId);
+    console.log(categoryId);
+
+    var query =
+      'SELECT ' +
+      'up.user as "user", ' +
+      'up.user_id as "user_id", ' +
+      'up.project as "projects", ' +
+      'up.project_description as "project_description", ' +
+      'up.project_id as "projects_id", ' +
+      'tc.category as "category", ' +
+      'tc.category_id as "category_id", ' +
+      'upt.task_description as "tasks", ' +
+      'upt.task_id as "tasks_id" ' +
+      'FROM ' +
+      '(SELECT u.user_name as "user", ' +
+      'u.id as "user_id", ' +
+      'p.project_name as "project", ' +
+      'p.description as "project_description", ' +
+      'p.id as "project_id" ' +
+      'FROM users u ' +
+      'LEFT JOIN project_users pu ' +
+      'ON u.id = pu.user_name JOIN projects p ON p.id = pu.project_name WHERE u.id = ' + userId + 
+      ' AND p.id = '+ categoryId +') up ' +
+      'LEFT JOIN ' +
+      '(SELECT u.id as "user", ' +
+      'tr.task_id as "task_id", ' +
+      't.task_project as "task_project_id", ' +
+      't.description as "task_description" ' +
+      'FROM users u LEFT JOIN tasks_responsibles tr ON tr.responsible = u.id LEFT JOIN tasks t ' +
+      'ON t.id = tr.task_id WHERE u.id = ' + userId + ') upt ' +
+      'ON upt.task_project_id = up.project_id ' +
+      'LEFT JOIN ' +
+      '(SELECT c.id as "category_id", ' +
+      'c.description as "category", ' +
+      't.id as "task_id", ' +
+      't.description as "task" ' +
+      'FROM categories c LEFT JOIN tasks t ' +
+      'ON t.task_category = c.id) tc ' +
+      'ON upt.task_id = tc.task_id'
+
+    connection.query(query, function (err, data) {
+
+      if (err) throw err;
+
+      res.json(data);
+
+      
+    });
+    
+    // }
+
+  });
+
 };
