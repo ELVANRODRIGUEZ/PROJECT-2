d3f4ed2595f085ded8aa0cbeaddee6464fb9318f
Ignacio Gaxiola <ignacioggb@hotmail.com>
2019-06-21 19:17:26 -0700

    pullreq

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ var syncOptions = {
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
 
     console.log("Hello!!!");
