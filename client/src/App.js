e4bab2dc08e4f02dd69219feddaf146668a097b7
ManuCas <manucastle@hotmail.com>
2019-07-30 21:39:15 -0500

    logout route fixed, navbar component completed, user name on navbar now shows

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Members from "./pages/Members";
@@ -9,7 +8,7 @@ import Members from "./pages/Members";
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
         <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={Signup} />
