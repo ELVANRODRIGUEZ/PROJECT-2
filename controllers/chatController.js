6ae97c6e0e64cb54239ad4162903b453459573ae
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2019-08-07 23:15:32 -0500

    Fixing to deploy.

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,7 +4,7 @@ module.exports = {
   
   findByTaskId(req, res) {
 
-    mongo.Chats.find({task: req.params.taskId}).sort({day: 1,hours: 1, minutes: 1})
+    mongo.Chats.find({taskId: req.params.taskId}).sort({day: 1,hours: 1, minutes: 1})
       .then(mongoChats => res.json(mongoChats))
       .catch((err) => {
         console.log(err);
